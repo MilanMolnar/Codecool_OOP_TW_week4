6633249b18487fb949a679c07073a6e7db320549
MilanMolnar <54602124+MilanMolnar@users.noreply.github.com>
2020-02-17 07:02:04 -0800

    Implementing XML Loader interface and implementation class

---
+++ CardGame/IDocumentLoader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame
+{
+    interface IDocumentLoader
+    {
+        
+        public List<Card> LoadCards(string path);
+    }
+}
--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CardGame
 {
@@ -6,7 +8,13 @@ namespace CardGame
     {
         static void Main(string[] args)
         {
-            
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/CardGame/Cards.xml");
+            XmlLoader xmlLoader = new XmlLoader();
+            List<Card> ListOfCards = xmlLoader.LoadCards(path);
+            foreach (var card in ListOfCards)
+            {
+                Console.WriteLine(card.Attack);
+            }
         }
     }
 }
