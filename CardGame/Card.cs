bcf3890650271059f90980abed5f49a1055667aa
Adam <borbelya90@gmail.com>
2020-02-17 15:14:05 +0100

    card class

---
+++ CardGame/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame
+{
+    class Card
+    {
+        public int HP { get; }
+        public int Attack { get; }
+        public int Defend { get; }
+        public int Speed { get; }
+
+        public Card(int hp, int attack, int defend, int speed)
+        {
+            HP = hp;
+            Attack = attack;
+            Defend = defend;
+            Speed = speed;
+        }
+    }
+}
