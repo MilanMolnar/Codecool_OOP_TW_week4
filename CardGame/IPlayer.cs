a3e912fc72ad6e1f7b6a267b48c05a13c7a1c33f
jakabij <jakabistvanj@gmail.com>
2020-02-17 15:28:24 +0100

    Player classes ready

---
+++ CardGame/IPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame
+{
+    interface IPlayer
+    {
+        int ChooseAttributes(Card topCard,string attribute);
+        void TakeCards(List<Card> topCards);
+    }
+}
