c643576c06ea040e842f0e0a4d64668a0c83b7a8
Adam <borbelya90@gmail.com>
2020-02-18 13:43:52 +0100

    Comparer

---
+++ CardGame/CardComparer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+
+namespace CardGame
+{
+    public class CardComparer
+    {
+        public class HPComparer : IComparer<Card>
+        {
+            public int Compare(Card x, Card y)
+            {
+                if (x.HP < y.HP)
+                {
+                    return 1;
+                }
+                else if (x.HP > y.HP)
+                {
+                    return -1;
+                }
+                else
+                    return 0;
+            }
+        }
+        public class AttackComparer : IComparer<Card>
+        {
+            public int Compare(Card x, Card y)
+            {
+                if (x.Attack < y.Attack)
+                {
+                    return 1;
+                }
+                else if (x.Attack > y.Attack)
+                {
+                    return -1;
+                }
+                else
+                    return 0;
+            }
+        }
+        public class DefendComparer : IComparer<Card>
+        {
+            public int Compare(Card x, Card y)
+            {
+                if (x.Defend < y.Defend)
+                {
+                    return 1;
+                }
+                else if (x.Defend > y.Defend)
+                {
+                    return -1;
+                }
+                else
+                    return 0;
+            }
+        }
+        public class SpeedComparer : IComparer<Card>
+        {
+            public int Compare(Card x, Card y)
+            {
+                if (x.Speed < y.Speed)
+                {
+                    return 1;
+                }
+                else if (x.Speed > y.Speed)
+                {
+                    return -1;
+                }
+                else
+                    return 0;
+            }
+        }
+    }
+}
