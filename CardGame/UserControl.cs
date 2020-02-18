3ea2047b20f1030afb1f9818ee9b8910dc9a4d6b
MilanMolnar <54602124+MilanMolnar@users.noreply.github.com>
2020-02-18 06:10:31 -0800

    Creating UserControl class and implementing main functionalities

---
+++ CardGame/UserControl.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame
+{
+    class UserControl
+    {
+        public void PrintPlayers(List<Player> players)
+        {
+            int count = 1;
+            foreach (var player in players)
+            {
+                Console.WriteLine($"[{count}.] Player ID: {player.Name}");
+                count++;
+            }
+        }
+        public void PrintPlayersTopCard(string name, List<Player> players)
+        {
+            foreach (var player in players)
+            {
+                if (player.Name == name)
+                {
+                    Console.WriteLine($"Player's ({player.Name}) top card: {player.listOfCards[0]}");
+                }
+                else
+                {
+                    Console.WriteLine("There is no naem that corresponds to the input");
+                }
+            }
+        }
+        public void PrintCards(List<Card> cards)
+        {
+            int count = 1;
+            foreach (var card in cards)
+            {
+                Console.WriteLine($"[{count}] Card's name: {card.Name}");
+            }
+        }
+        public void PrintCardWithAttributes(Card card)
+        {
+            Console.WriteLine($"Card Name: {card.Name}:");
+            Console.WriteLine($"\tHP: {card.HP}");
+            Console.WriteLine($"\tAttack: {card.Attack}");
+            Console.WriteLine($"\tDeffense: {card.Defend}");
+            Console.WriteLine($"\tSpeed: {card.Speed}");
+        }
+        public int AskPlayersForNumOfPlayer()
+        {
+            Console.Write("How many players wants to play: ");
+            int numOfPlayer = Convert.ToInt32(Console.ReadLine());
+            return numOfPlayer;
+        }
+        public int AskForBotPlayers()
+        {
+            Console.Write("How many of them are bots: ");
+            int numOfBotPlayers = Convert.ToInt32(Console.ReadLine());
+            return numOfBotPlayers;
+        }
+        public string ChooseAttributePrompt(Card topCard)
+        {
+            PrintCardWithAttributes(topCard);
+            Console.WriteLine();
+            Console.Write("Decide which attribute you want to fight with: ");
+            return Console.ReadLine();
+        }
+        public void PrintWinner(Player winner)
+        {
+            Console.WriteLine($"The winner is {winner.Name}!");
+        }
+    }
+}
--- CardGame/XmlLoader.cs
+++ CardGame/XmlLoader.cs
@@ -14,14 +14,15 @@ namespace CardGame
             List<Card> ListOfCards = new List<Card>();
             XElement element = XElement.Load(path);//string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/CardGame/Cards.xml");
             var cardNodes = element.Elements("Card");
-            int hp; int atk; int def; int speed;
+            int hp; int atk; int def; int speed; string name;
             foreach (var node in cardNodes)
             {
                 hp = Convert.ToInt32(node.Element("HP").Value);
                 atk = Convert.ToInt32(node.Element("Atk").Value);
                 def = Convert.ToInt32(node.Element("Def").Value);
                 speed = Convert.ToInt32(node.Element("Speed").Value);
-                Card card = new Card(hp, atk, def, speed);
+                name = node.Element("Name").Value;
+                Card card = new Card(hp, atk, def, speed, name);
                 ListOfCards.Add(card);
             }
             return ListOfCards;
